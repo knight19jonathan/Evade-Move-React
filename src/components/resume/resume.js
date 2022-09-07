42fb810c039f24e56cca8f58fe3490c4280e503c
Magnus Did Nothing Wrong <knight19jonathan@gmail.com>
2022-09-06 19:33:42 -0700

    resume

--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -6,6 +6,80 @@ function Resume(){
     return(
         <div className='text-left'>
             <h1 className='title'>Resume</h1>
+            <p>
+            Jonathan T. Knight 
+2841 13th Ave W, Seattle, WA, 98119
+(254) 383-7018  ∙  knight19jonathan@gmail.com
+
+Qualifications Profile
+
+Full Stack Web Developer with a background in customer service. 
+Apprentice in HTML, CSS, JavaScript, jQuery, Node.js, Express.js, MySQL, MongoDB, React.js, and responsive web design.
+Over four years of proven leadership skills and supervisory experience; adept at overseeing day-to-day operations of staff and motivating employees.
+Experienced Emergency Medical Technician working for American Medical Response for twenty months.
+Resourceful problem solver capable of implementing solutions to complex problems.
+Experienced with interacting with patients and conducting work professionally in high stress situations.
+Outstanding written and oral communication skills; practiced at preparing detailed reports, delivering concise briefings, and conveying complex information clearly to stakeholders at all levels of management.
+
+Professional Experience
+
+American Medical Response	Seattle, WA
+Basic Emergency Medical Technician March 2016 – Present 
+•    Perform rigorous physical and mental tasks under pressure.	 
+•    Accurately document and report all daily professional activities. 
+•    Assess customer needs, provide customer service corresponding with King County and WA State policy and medical controls.
+Security Professional     	Seattle, WA
+Tia Lou’s Night Club, May 2015 –  January 2017 
+•     Perform as a customer service representative as an entertainment establishment. 
+•     Maintain a high degree of situational awareness to prevent and de-escalate conflicts.	
+United States Army  	    Fort Hood, TX
+Plans Officer, August 2013 – February 2015 
+•	Manage projects in accordance with quarterly and annual target goals.  
+•	Track daily progress of subordinate organizations aligns with completion of target goals. 
+•     Prepared information for consumption of superiors to make decisions.
+•	Conduct Couse of Action Analysis using the fundamental of the Military Decision Making Process. 
+United States Army	Fort Hood, TX
+Combat & Brigade Engineer Operations Officer, Asst. Operations Officer, July 2012 – August 2013
+•	Provide information to customers based of professional working knowledge and information groups. 
+•	Conducted capability analysis of specific organizational capabilities to the overall organization and how those contribute to the organizational target goal. 
+United States Army	Fort Hood TX and Talil, Iraq
+Platoon Leader, September 2010 –July 2012
+•	Supervisor and manage of 32 Soldiers in the United States Army and the responsible party for over $20 million dollars in equipment.
+•	Trained, prepared, and deployed to an active Combat Zone in Operation New Dawn in support of the resolution of the Iraq War.
+•     Conducted over 150 patrol missions in support of combat operations. 	 
+
+Education and Credentials
+
+Bachelor of Arts in Political Science – 2009
+Central Washington University – Ellensburg, WA
+
+
+U.S. Army Combatives Training Facility	Fort Hood, TX
+Basic Combatives Course (July 2014) [Physical Education Opportunity] 
+Tactical Combatives Course (November 2014) [Advanced Physical Education Opportunity]
+
+U.S. Army National Training Center	Fort Irwin, CA
+Observer, Coach, and Trainer Academy Graduate (February 2014) [Mentoring and Coaching Opportunity]
+
+U.S. Army Counter Explosives Hazards Center	Fort Lenoardwood, MO
+Engineer Explosive Ordnance Clearance Agent Certification (August 2010) [Technical Training Oppporuntunity in Robotics and Explosives] 
+
+U.S. Army Engineer Basic Officer Leader’s Course	Fort Lenoardwood, MO
+Officer Branch Qualification (July 2010) [Leadership and Educational Experience]
+
+Central Washington University	Ellensburg, WA
+Reserve Officer Training Corps Commission (June 2009) [Collegiate Educational and Professional Qualification Course] 
+
+U.S. Army Maneuver Center For Excellence 	Fort Benning, GA
+Basic Airborne Certification (July 2007) [Personal Courage Experience] 
+
+Basic Emergency Medical Service Technician Course 	Seattle, WA
+Graduated from North Seattle Community College Course (December 2015) [Professional Qualification Course]
+National EMS-B Certificate (January 2016) [National Certification]
+Washington State EMS-B Certificate (August 2016) [Sate Certifcation]
+
+
+            </p>
             <a href='https://docs.google.com/document/d/1O2J_bfFt8x1dZ868cV-cuYK8pbfcWKbv/edit?usp=sharing&ouid=106037536118220566833&rtpof=true&sd=true' target='_blank' rel='noopener noreferrer'>
                 <button className='btn btn-primary'>View Resume</button>
             </a>
